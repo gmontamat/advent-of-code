61019abcd64339b713838a581aa35c28db88e595
Gustavo Montamat <gmontamat@gmail.com>
2026-01-04 08:53:41 -0500

    Complete AoC 2024 day 5

---
+++ python/2024/05/solution1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#     "networkx",
+# ]
+# ///
+
+import sys
+
+import networkx as nx
+
+
+def main(edges, seqs):
+    # Treating as a topological sorting problem
+    total = 0
+    for sequence in seqs:
+        g_edges = [(a, b) for a, b in edges if a in sequence and b in sequence]
+        graph = nx.DiGraph(g_edges)
+        try:
+            all_sorts_generator = nx.all_topological_sorts(graph)
+            list_of_sorts = list(all_sorts_generator)
+        except networkx.exception.NetworkXUnfeasible:
+            # There is a cycle!
+            continue
+        if sequence in list_of_sorts:
+            # valid!
+            total += sequence[len(sequence) // 2]
+    return total
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: solution1.py <file>")
+        sys.exit(0)
+    file_path = sys.argv[1]
+    edges = []
+    sequences = []
+    load_graph = True
+    with open(file_path, "r") as fin:
+        for line in fin:
+            if not line.strip():
+                load_graph = False
+                continue
+            if load_graph:
+                n1, n2 = line.strip().split("|")
+                edges.append((int(n1), int(n2)))
+            else:
+                sequences.append([int(n) for n in line.strip().split(",")])
+    print(main(edges, sequences))
