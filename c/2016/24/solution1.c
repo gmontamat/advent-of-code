0b3bbb92d02dc4cf029d4d117400b0e3ede3bb46
Gustavo Montamat <gmontamat@gmail.com>
2026-05-29 00:42:34 -0400

    Complete A* part

--- c/2016/24/solution1.c
+++ c/2016/24/solution1.c
@@ -1,5 +1,3 @@
-#include <cstddef>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -7,6 +5,9 @@
 
 #define STB_DS_IMPLEMENTATION
 #include "../thirdparty/stb_ds.h"
+
+#define BUFFER_SIZE 256
+#define INF 9999999
 
 typedef struct {
     int32_t x;
@@ -16,47 +17,87 @@ typedef struct {
 int32_t height = 6;
 int32_t width = 11;
 
-Position getNodePosition(char grid[][width], char node) {
-    for (int32_t i = 0; i < height; ++ i) {
-        for (int32_t j = 0; j < width; ++j) {
-            if (grid[i][j] == node) return (Position){i, j};
-        }
-    }
-    printf("node %c not found!", node);
-    exit(1);
+int32_t estimateDistance(Position a, Position b) {
+    return abs(a.x - b.x) + abs(a.y - b.y);
 }
 
-int32_t estimateDistance(char grid[][width], Position a, Position b) {
-    return abs(a.x - b.x) + abs(a.y - b.y);
+Position getNeighbor(Position current, size_t i) {
+    Position neighbor = current;
+    switch (i) {
+        case 0:
+            neighbor.x++;
+            return neighbor;
+        case 1:
+            neighbor.x--;
+            return neighbor;
+        case 2:
+            neighbor.y++;
+            return neighbor;
+        case 3:
+            neighbor.y--;
+            return neighbor;
+    }
+    return neighbor;
 }
 
 int32_t astar(char grid[][width], Position start, Position goal) {
     // https://en.wikipedia.org/wiki/A*_search_algorithm
-    struct { Position key; _Bool value; } *open_set = NULL;
+    struct { Position key; _Bool value; } *open_set = NULL;  // slow, replace with priority queue
+    hmdefault(open_set, 0);
     hmput(open_set, start, 1);
     struct { Position key; int32_t value; } *g_score = NULL;
-    hmdefault(g_score, INT32_MAX);
+    hmdefault(g_score, INF);
     hmput(g_score, start, 0);
     struct { Position key; int32_t value; } *f_score = NULL;
-    hmdefault(f_score, INT32_MAX);
-    hmput(f_score, start, estimateDistance(grid, start, goal));
+    hmdefault(f_score, INF);
+    hmput(f_score, start, estimateDistance(start, goal));
     while (1) {
         if (hmlen(open_set) == 0) break;
-        //         current := the node in open_set having the lowest f_score[] value
-        int32_t lowest = INT32_MAX;
-        Position current = {0, 0};
+        // current is the node in open_set having the lowest f_score[] value
+        int32_t lowest = INF;
+        Position current = {-1, -1};
         for (ptrdiff_t i = 0; i < hmlen(f_score); ++i) {
-            if (hmgeti(f_score,))
+            if (hmget(open_set, f_score[i].key) == 0) continue;
+            if (f_score[i].value < lowest) {
+                lowest = f_score[i].value;
+                current = f_score[i].key;
+            }
+        }
+        // is it the goal?
+        if (current.x == goal.x && current.y == goal.y) return hmget(g_score, goal);
+        hmput(open_set, current, 0);
+        for (size_t i = 0; i < 4; ++i) {
+            Position neighbor = getNeighbor(current, i);
+            if (neighbor.x >= 0 && neighbor.x < width &&
+                    neighbor.y >= 0 && neighbor.y < height &&
+                    grid[neighbor.y][neighbor.x] != '#') {
+                int32_t tentative_g = hmget(g_score, current) + 1;
+                if (tentative_g < hmget(g_score, neighbor)) {
+                    hmput(g_score, neighbor, tentative_g);
+                    hmput(f_score, neighbor, tentative_g + estimateDistance(neighbor, goal));
+                    if (hmget(open_set, neighbor) == 0) hmput(open_set, neighbor, 1);
+                }
+            }
+        }
+
+    }
+    return INT32_MAX; // failure to find a path
+}
+
+Position getNodePosition(char grid[][width], char node) {
+    for (int32_t i = 0; i < height; ++i) {
+        for (int32_t j = 0; j < width; ++j) {
+            if (grid[i][j] == node) return (Position){j, i};
         }
     }
-    return hmget(g_score, goal); // failure to find a path
+    printf("node %c not found...\n", node);
+    exit(1);
 }
 
 int32_t minDistance(char grid[][width], char nodea, char nodeb) {
     Position a = getNodePosition(grid, nodea);
     Position b = getNodePosition(grid, nodeb);
-
-
+    return astar(grid, a, b);
 }
 
 int main(int argc, char **argv) {
@@ -70,12 +111,28 @@ int main(int argc, char **argv) {
     // and DFS to pick best order of nodes from 0
     if (strcmp(argv[1], "input") == 0) height = 45, width = 177; 
     char grid[height][width];
+    char buffer[BUFFER_SIZE];
+    size_t row = 0;
 
-    while (fscanf(f, "%d %d %d", &a, &b, &c) == 3) {
-        if ((a + b) > c && (a + c) > b && (c + b) > a) count++;
+    while (fgets(buffer, BUFFER_SIZE, f) != NULL) {
+        buffer[strcspn(buffer, "\n")] = '\0';  // Remove trailing newline
+        for (size_t i = 0; i < (size_t)width; ++i) {
+            grid[row][i] = buffer[i];
+        }
+        row++;
     }
     fclose(f);
 
-    printf("%d\n", count);
+    for (size_t i = 0; i < (size_t)height; ++i) {
+        for (size_t j = 0; j < (size_t)width; ++j) {
+            printf("%c", grid[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("%d\n", minDistance(grid, '0', '4'));
+    printf("%d\n", minDistance(grid, '4', '1'));
+    printf("%d\n", minDistance(grid, '1', '2'));
+    printf("%d\n", minDistance(grid, '2', '3'));
     return 0;
 }
