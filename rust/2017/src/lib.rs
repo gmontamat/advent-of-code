0df6221b7332551182a62b56288644623e06dd5f
Gustavo Montamat <gmontamat@gmail.com>
2026-06-26 16:50:22 -0400

    Search inputs/examples using CARGO_MANIFEST_DIR

--- rust/2017/src/lib.rs
+++ rust/2017/src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn read_input(day: u8) -> String {
-    let path = format!("inputs/day{:02}.txt", day);
+    let path = format!("{}/inputs/day{:02}.txt", env!("CARGO_MANIFEST_DIR"), day);
     fs::read_to_string(&path)
         .unwrap_or_else(|_| panic!("Failed to read input file: {}", path))
         .trim()
@@ -9,7 +9,7 @@ pub fn read_input(day: u8) -> String {
 }
 
 pub fn read_example(day: u8) -> String {
-    let path = format!("examples/day{:02}.txt", day);
+    let path = format!("{}/examples/day{:02}.txt", env!("CARGO_MANIFEST_DIR"), day);
     fs::read_to_string(&path)
         .unwrap_or_else(|_| panic!("Failed to read example file: {}", path))
         .trim()
@@ -17,7 +17,7 @@ pub fn read_example(day: u8) -> String {
 }
 
 pub fn read_inputs(day: u8) -> Vec<String> {
-    let path = format!("inputs/day{:02}.txt", day);
+    let path = format!("{}/inputs/day{:02}.txt", env!("CARGO_MANIFEST_DIR"), day);
     fs::read_to_string(&path)
         .unwrap_or_else(|_| panic!("Failed to read input file: {}", path))
         .lines()
@@ -26,7 +26,7 @@ pub fn read_inputs(day: u8) -> Vec<String> {
 }
 
 pub fn read_examples(day: u8) -> Vec<String> {
-    let path = format!("examples/day{:02}.txt", day);
+    let path = format!("{}/examples/day{:02}.txt", env!("CARGO_MANIFEST_DIR"), day);
     fs::read_to_string(&path)
         .unwrap_or_else(|_| panic!("Failed to read example file: {}", path))
         .lines()
